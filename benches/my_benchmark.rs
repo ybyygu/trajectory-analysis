c8adb6aed0a86c94a37d4b7c897d7f7f26a3c834
Wenping Guo <ybyygu@gmail.com>
2023-02-01 21:30:20 +0800

    bench for traj parsing

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -4,8 +4,8 @@ use trajectory_analysis::docs::xyztraj::*;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-// const input_file: &'static str = "data/55/798fcc-6c7a-424f-8c87-7e8b11300345/输入和输出轨迹/15Ca300k-pos-1.xyz";
-const input_file: &'static str = "data/55/798fcc-6c7a-424f-8c87-7e8b11300345/SiAlCaO1800k.xyz";
+const input_file: &'static str = "data/55/798fcc-6c7a-424f-8c87-7e8b11300345/输入和输出轨迹/15Ca300k-pos-1.xyz";
+// const input_file: &'static str = "data/55/798fcc-6c7a-424f-8c87-7e8b11300345/SiAlCaO1800k.xyz";
 
 fn read_xyz_traj_raw() {
     let mols = gchemol::io::read(input_file).unwrap();
@@ -17,9 +17,10 @@ fn read_xyz_traj_opt() {
     let _ = mols.count();
 }
 
+
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("read-xyz-traj-opt", |b| b.iter(|| read_xyz_traj_opt()));
     c.bench_function("read-xyz-traj-raw", |b| b.iter(|| read_xyz_traj_raw()));
+    c.bench_function("read-xyz-traj-opt", |b| b.iter(|| read_xyz_traj_opt()));
 }
 
 criterion_group!(benches, criterion_benchmark);
